a098c182cc1803cffe2306880471dff51e7090d9
aa0958121915@outlook.com <aa8952eaya>
2016-08-23 17:07:47 +0800

    Pockmon asskid

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -3,13 +3,38 @@ package com.example.user.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView infoText;
+    EditText nameEditText;
+    RadioGroup optionGroup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_main);
+
+        infoText = (TextView)findViewById(R.id.inforText);
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        optionGroup = (RadioGroup)findViewById(R.id.optionGroup);
+        Button confirmBtn = (Button)findViewById(R.id.confirmButton);
+        confirmBtn.setOnClickListener(MainActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if(viewId == R.id.confirmButton);
+            Log.d("buttonText","123456789");
+
 
     }
 }
